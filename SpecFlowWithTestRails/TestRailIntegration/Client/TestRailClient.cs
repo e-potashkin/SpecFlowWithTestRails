1a4952437634000b23af3437f3af9e79177421c4
Eugene Potashkin <yevhenii.potashkin@arvosoftware.com>
2023-07-08 14:46:13 +0300

    Add retry policy for TestRailClient
    Added a retry policy to the TestRailClient for handling transient errors. RetryPolicyBuilder class is introduced to create a retry policy. The policy can handle exceptions like RequestTimeout, BadGateway, ServiceUnavailable and GatewayTimeout by executing retries with exponential backoff. The Policy package reference was included to the project to implement the retry policies.

--- SpecFlowWithTestRails/TestRailIntegration/Client/TestRailClient.cs
+++ SpecFlowWithTestRails/TestRailIntegration/Client/TestRailClient.cs
@@ -1,21 +1,34 @@
 using Flurl.Http;
 using SpecFlowWithTestRails.Environment;
+using SpecFlowWithTestRails.TestRailIntegration.Resilience;
 
 namespace SpecFlowWithTestRails.TestRailIntegration.Client;
 
 public sealed class TestRailClient : ITestRailClient
 {
+    private readonly RetryPolicyBuilder _retryPolicyBuilder;
     private readonly EnvironmentSettings _settings;
 
-    public TestRailClient(EnvironmentSettings settings) => _settings = settings;
+    public TestRailClient(RetryPolicyBuilder retryPolicyBuilder, EnvironmentSettings settings)
+    {
+        _retryPolicyBuilder = retryPolicyBuilder;
+        _settings = settings;
+    }
 
-    public async Task<IFlurlResponse> GetAsync(string uri) =>
-       await $"{_settings.TestRailBaseUrl}{uri}"
-           .WithBasicAuth(_settings.TestRailUser, _settings.TestRailPassword)
-           .GetAsync();
-
-    public async Task<IFlurlResponse> PostAsync(string uri, object data) =>
-        await $"{_settings.TestRailBaseUrl}{uri}"
+    public async Task<IFlurlResponse> GetAsync(string uri)
+    {
+        var policy = _retryPolicyBuilder.BuildRetryPolicy();
+        return await policy.ExecuteAsync(() => $"{_settings.TestRailBaseUrl}{uri}"
             .WithBasicAuth(_settings.TestRailUser, _settings.TestRailPassword)
-            .PostJsonAsync(data);
+            .GetAsync()
+        );
+    }
+
+    public async Task<IFlurlResponse> PostAsync(string uri, object data)
+    {
+        var policy = _retryPolicyBuilder.BuildRetryPolicy();
+        return await policy.ExecuteAsync(() => $"{_settings.TestRailBaseUrl}{uri}"
+            .WithBasicAuth(_settings.TestRailUser, _settings.TestRailPassword)
+            .PostJsonAsync(data));
+    }
 }
